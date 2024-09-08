e2637c012e9b5f39f1afc68a0f5df0c6744d8aaf
Isabela Ramos <isamrteixeira@gmail.com>
2024-09-07 21:56:45 -0300

    tipagem do botão

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,18 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 import { colors } from '../../styles'
 
 export const ButtonContainer = styled.button`
+  background-color: tranparent;
+  color: ${colors.white};
+  font-size: 16px;
+  font-weight: bold;
+  border: 2px solid ${colors.white};
+  border-radius: 8px;
+  padding: 8px 16px;
+`
+
+export const ButtonLink = styled(Link)`
   background-color: tranparent;
   color: ${colors.white};
   font-size: 16px;
