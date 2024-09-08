64bddda8feda35ea1c8df8fad8c253d56de8a362
Isabela Ramos <isamrteixeira@gmail.com>
2024-09-07 23:46:53 -0300

    organizando Router em páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { GlobalStyle } from './styles'
-import Banner from './components/Banner'
+import Home from './pages/Home'
 import Header from './components/Header'
-import ProductsList from './components/ProductsList'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: (
-      <>
-        <Banner />
-        <ProductsList title="Promoções" background="gray" />
-        <ProductsList title="Em breve" background="black" />
-      </>
-    )
+    element: <Home />
   }
 ])
 
