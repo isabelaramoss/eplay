8244d10f7af260aec39533bcb8c09fb6979409fd
Isabela Ramos <isamrteixeira@gmail.com>
2024-09-08 00:53:04 -0300

    tornando a logo em link para Home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,9 @@ import cart from '../../assets/images/carrinho.svg'
 const Header = () => (
   <HeaderBar>
     <div>
-      <img src={logo} alt="Logo EPLAY" />
+      <Link to="/">
+        <img src={logo} alt="Logo EPLAY" />
+      </Link>
       <nav>
         <Links>
           <LinkItem>
