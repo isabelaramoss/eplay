537d4723a2c3e711637dd87e570a80d15e72c472
Isabela Ramos <isamrteixeira@gmail.com>
2024-09-15 16:52:15 -0300

    nova rota para página produto

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Categorias from './pages/Categorias'
+import Product from './pages/Product'
 
 /* const router = createBrowserRouter([
     {
@@ -17,6 +18,7 @@ const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categorias" element={<Categorias />} />
+    <Route path="/produto/:id" element={<Product />} />
   </Routes>
 )
 
