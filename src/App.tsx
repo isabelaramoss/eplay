37eea126e8084dc4d14c81816ea77ec36f5891d0
Isabela Ramos <isabela.devfront@gmail.com>
2024-10-22 11:04:57 -0300

    feat: adiciona reducer, slice e provider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles'
 import Rotas from './routes'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <div className="container">
-        <Header />
-      </div>
-      <Rotas />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <div className="container">
+          <Header />
+        </div>
+        <Rotas />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
