85f698810707380afdc2bcd3dc6f939f01fabcf1
Isabela Ramos <isabela.devfront@gmail.com>
2024-10-27 14:49:47 -0300

    feat: adiciona Cart aos elementos globais

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Rotas from './routes'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { store } from './store'
+import Cart from './components/Cart'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         </div>
         <Rotas />
         <Footer />
+        <Cart />
       </BrowserRouter>
     </Provider>
   )
